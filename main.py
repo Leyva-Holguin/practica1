2da4c733e1a2c36589515d6219ceb9b4d8836b52
Daniel ivan <24308060610008@cetis61.edu.mx>
2026-03-23 11:17:10 -0500

    1

---
+++ main.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError, ConnectionFailure
+from bson.objectid import ObjectId
+from datetime import datetime, timedelta
+from typing import Optional, List, Dict
+import os
+
+def main():
+    print("Hello from practica1!")
+
+
+if __name__ == "__main__":
+    main()
